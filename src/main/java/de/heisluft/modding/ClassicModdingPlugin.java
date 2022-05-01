8c9da9c9770c5980a1e89a87ded3f6f2a1e8f732
heisluft <heisluftlp@gmail.com>
2022-05-01 17:46:18 +0200

    Add some more tasks, next step: decompilation

--- src/main/java/de/heisluft/modding/ClassicModdingPlugin.java
+++ src/main/java/de/heisluft/modding/ClassicModdingPlugin.java
@@ -4,8 +4,7 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginExtension;
-import org.gradle.api.tasks.TaskContainer;
-import org.gradle.api.tasks.TaskProvider;
+import org.gradle.api.tasks.*;
 import org.gradle.api.tasks.compile.JavaCompile;
 import org.gradle.jvm.toolchain.JavaLanguageVersion;
 import org.gradle.jvm.toolchain.JvmVendorSpec;
@@ -34,15 +33,17 @@ public class ClassicModdingPlugin implements Plugin<Project> {
       javaCompile.setTargetCompatibility("1.5");
       javaCompile.setSourceCompatibility("1.5");
     });
+    // Maven Central hosts launchwrappers dependencies
     project.getRepositories().mavenCentral();
+    // heisluft.de hosts the latest launchwrapper compiled for java 5
     project.getRepositories().maven(repo -> repo.setUrl("https://heisluft.de/maven/"));
+    // launchwrapper is used to start mc
     project.getDependencies().add("implementation", "net.minecraft:launchwrapper:1.12");
     Ext ext = project.getExtensions().create("classicMC", Ext.class);
 
     TaskContainer tasks = project.getTasks();
     TaskProvider<MavenJarDownloadTask> downloadDeobfTools = tasks.register("downloadDeobfTools", MavenJarDownloadTask.class, task -> {
       task.getArtifactLocation().set("de.heisluft.reveng:RevEng");
-      task.getVersion().set("0.0.1");
       task.getClassifier().set("all");
       task.getMavenRepoUrl().set(REPO_URL);
     });
@@ -55,6 +56,42 @@ public class ClassicModdingPlugin implements Plugin<Project> {
       task.dependsOn(downloadMC);
       task.getInput().set(downloadMC.get().getOutput());
       task.getIncludedPaths().addAll(Arrays.asList("**.png", "**.gif"));
+    });
+    TaskProvider<Zip2ZipCopyTask> stripLibs = tasks.register("stripLibraries", Zip2ZipCopyTask.class, task -> {
+      task.dependsOn(downloadMC);
+      task.getInput().set(downloadMC.get().getOutput());
+      task.getIncludedPaths().addAll(Arrays.asList("a/**", "com/**"));
+    });
+    TaskProvider<OutputtingJavaExec> fixConstructors = tasks.register("fixConstructors", OutputtingJavaExec.class, task -> {
+      task.dependsOn(stripLibs, downloadDeobfTools);
+      task.classpath(downloadDeobfTools.get().getOutput().get());
+      task.setOutputFilename("minecraft.jar");
+      task.getMainClass().set("de.heisluft.reveng.ConstructorFixer");
+      task.args(stripLibs.get().getOutput().get().getAsFile().getAbsolutePath(), task.getOutput().get().getAsFile().getAbsolutePath());
+    });
+    TaskProvider<OutputtingJavaExec> generateMappings = tasks.register("generateMappings", OutputtingJavaExec.class, task -> {
+      task.dependsOn(stripLibs, downloadDeobfTools);
+      task.classpath(downloadDeobfTools.get().getOutput().get());
+      task.setOutputFilename("generated.frg");
+      task.getMainClass().set("de.heisluft.reveng.Remapper");
+      task.args(
+          "map",
+          stripLibs.get().getOutput().get().getAsFile().getAbsolutePath(),
+          task.getOutput().get().getAsFile().getAbsolutePath()
+      );
+    });
+    TaskProvider<OutputtingJavaExec> remapJar = tasks.register("remapJar", OutputtingJavaExec.class, task -> {
+      task.dependsOn(generateMappings, fixConstructors);
+      task.classpath(downloadDeobfTools.get().getOutput().get());
+      task.setOutputFilename("minecraft.jar");
+      task.getMainClass().set("de.heisluft.reveng.Remapper");
+      task.args(
+          "remap",
+          fixConstructors.get().getOutput().get().getAsFile().getAbsolutePath(),
+          generateMappings.get().getOutput().getAsFile().get().getAbsolutePath(),
+          "-o",
+          task.getOutput().get().getAsFile().getAbsolutePath()
+      );
     });
   }
 }
