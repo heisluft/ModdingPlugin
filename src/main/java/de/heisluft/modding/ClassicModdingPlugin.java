f0f9991dd2629776da0cd01251fd9b0e22508467
heisluft <heisluftlp@gmail.com>
2022-07-13 22:23:29 +0200

    use separate sourceset for mc source code, (allow assets to be referenced from main), use java11 explicitly for decomp and java 8 for compilation of mc sources with j1.5 target compat. integrate the new inner class restoration, remove explicit legacylauncher dep

--- src/main/java/de/heisluft/modding/ClassicModdingPlugin.java
+++ src/main/java/de/heisluft/modding/ClassicModdingPlugin.java
@@ -2,6 +2,7 @@ package de.heisluft.modding;
 
 import de.heisluft.modding.repo.ResourceRepo;
 import de.heisluft.modding.tasks.*;
+import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.dsl.DependencyHandler;
@@ -11,15 +12,24 @@ import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.tasks.*;
 import org.gradle.api.tasks.compile.JavaCompile;
 import org.gradle.jvm.toolchain.JavaLanguageVersion;
+import org.gradle.jvm.toolchain.JavaToolchainService;
+import org.gradle.jvm.toolchain.JavaToolchainSpec;
 import org.gradle.jvm.toolchain.JvmVendorSpec;
 
 import java.io.File;
 import java.util.Arrays;
-import java.util.Set;
+import java.util.Collections;
 
 public class ClassicModdingPlugin implements Plugin<Project> {
 
   public static final String REPO_URL = "https://heisluft.de/maven/";
+
+  private static Action<JavaToolchainSpec> versionOf(int version) {
+    return javaToolchainSpec -> {
+      javaToolchainSpec.getLanguageVersion().set(JavaLanguageVersion.of(version));
+      javaToolchainSpec.getVendor().set(JvmVendorSpec.ADOPTOPENJDK);
+    };
+  }
 
   @Override
   public void apply(Project project) {
@@ -29,26 +39,30 @@ public class ClassicModdingPlugin implements Plugin<Project> {
     project.getPluginManager().apply(JavaPlugin.class);
     // Java 8 is the only version to still be available and being able to compile java 5. hooray.
     JavaPluginExtension javaExt = project.getExtensions().getByType(JavaPluginExtension.class);
-    javaExt.toolchain(it -> {
-      it.getLanguageVersion().set(JavaLanguageVersion.of(8));
-      it.getVendor().set(JvmVendorSpec.ADOPTOPENJDK);
-    });
-    // We need to ensure binary compat with mojang. sorry, no NIO, Diamonds or lambdas for us.
+    javaExt.toolchain(versionOf(16));
+    JavaToolchainService service = project.getExtensions().getByType(JavaToolchainService.class);
+    SourceSet mcSourceSet = javaExt.getSourceSets().maybeCreate("mc");
+    // We dont want things like modlauncher to be available to the mc source code
+    // so implementation will not cover that
+    project.getConfigurations().getByName("implementation").setExtendsFrom(Collections.singleton(project.getConfigurations().getByName("mcImplementation")));
     project.getTasks().withType(JavaCompile.class).forEach(javaCompile -> {
       javaCompile.getOptions().setEncoding("UTF-8");
-      javaCompile.setTargetCompatibility("1.5");
-      javaCompile.setSourceCompatibility("1.5");
+      // We want to ensure basic binary compatability with mojang.
+      // Sorry, no NIO, Diamonds or lambdas for us (at least in the mc sourceSet :D).
+      if(javaCompile.getName().equals("compileMcJava")) {
+        javaCompile.getJavaCompiler().set(service.compilerFor(versionOf(8)));
+        javaCompile.setTargetCompatibility("1.5");
+        javaCompile.setSourceCompatibility("1.5");
+      }
     });
     // Maven Central hosts launchwrappers and mcs dependencies
     project.getRepositories().mavenCentral();
     // heisluft.de hosts the latest launchwrapper compiled for java 5 as well as jogg
     project.getRepositories().maven(repo -> repo.setUrl("https://heisluft.de/maven/"));
     DependencyHandler d = project.getDependencies();
-    d.add("implementation", "net.minecraft:launchwrapper:1.12");
-    d.add("implementation", "org.lwjgl.lwjgl:lwjgl:2.9.3");
-    d.add("implementation", "org.lwjgl.lwjgl:lwjgl_util:2.9.3");
-    d.add("implementation", "de.jarnbjo:j-ogg-mc:1.0.1");
-    d.add("implementation", "de.heisluft.classiclaunch:launch:1.0.0");
+    d.add("mcImplementation", "org.lwjgl.lwjgl:lwjgl:2.9.3");
+    d.add("mcImplementation", "org.lwjgl.lwjgl:lwjgl_util:2.9.3");
+    d.add("mcImplementation", "de.jarnbjo:j-ogg-mc:1.0.1");
 
     Ext ext = project.getExtensions().create("classicMC", Ext.class);
 
@@ -76,23 +90,23 @@ public class ClassicModdingPlugin implements Plugin<Project> {
       task.getExtension().set("zip");
       task.getMavenRepoUrl().set(REPO_URL);
     });
-    TaskProvider<OutputtingJavaExec> fixConstructors = tasks.register("fixConstructors", OutputtingJavaExec.class, task -> {
+    TaskProvider<OutputtingJavaExec> fixInners = tasks.register("fixInners", OutputtingJavaExec.class, task -> {
       task.dependsOn(downloadMC, downloadDeobfTools);
       task.classpath(downloadDeobfTools.get().getOutput().get());
       task.setOutputFilename("minecraft.jar");
-      task.getMainClass().set("de.heisluft.reveng.ConstructorFixer");
+      task.getMainClass().set("de.heisluft.reveng.nests.InnerClassDetector");
       task.args(
           downloadMC.get().getOutput().get().getAsFile().getAbsolutePath(),
           task.getOutput().get().getAsFile().getAbsolutePath()
       );
     });
-    TaskProvider<OutputtingJavaExec> fixEnumSwitches = tasks.register("fixEnumSwitches", OutputtingJavaExec.class, task -> {
-      task.dependsOn(fixConstructors);
+    TaskProvider<OutputtingJavaExec> fixConstructors = tasks.register("fixConstructors", OutputtingJavaExec.class, task -> {
+      task.dependsOn(fixInners);
       task.classpath(downloadDeobfTools.get().getOutput().get());
       task.setOutputFilename("minecraft.jar");
-      task.getMainClass().set("de.heisluft.reveng.nests.EnumSwitchClassDetector");
+      task.getMainClass().set("de.heisluft.reveng.ConstructorFixer");
       task.args(
-          fixConstructors.get().getOutput().get().getAsFile().getAbsolutePath(),
+          fixInners.get().getOutput().get().getAsFile().getAbsolutePath(),
           task.getOutput().get().getAsFile().getAbsolutePath()
       );
     });
@@ -102,13 +116,13 @@ public class ClassicModdingPlugin implements Plugin<Project> {
       task.getIncludedPaths().addAll(Arrays.asList("fergie.frg", "at.cfg", "patches/*"));
     });
     TaskProvider<OutputtingJavaExec> remapJar = tasks.register("remapJar", OutputtingJavaExec.class, task -> {
-      task.dependsOn(extractData, fixEnumSwitches);
+      task.dependsOn(extractData, fixConstructors);
       task.classpath(downloadDeobfTools.get().getOutput().get());
       task.setOutputFilename("minecraft.jar");
       task.getMainClass().set("de.heisluft.reveng.Remapper");
       task.args(
           "remap",
-          fixEnumSwitches.get().getOutput().get().getAsFile().getAbsolutePath(),
+          fixConstructors.get().getOutput().get().getAsFile().getAbsolutePath(),
           new File(extractData.get().getOutput().getAsFile().get(), "fergie.frg").getAbsolutePath(),
           "-o",
           task.getOutput().get().getAsFile().getAbsolutePath()
@@ -136,6 +150,7 @@ public class ClassicModdingPlugin implements Plugin<Project> {
       task.classpath(downloadFernFlower.get().getOutput().get());
       task.getMainClass().set("org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler");
       task.setMaxHeapSize("4G");
+      task.getJavaLauncher().set(service.launcherFor(versionOf(11)));
       task.args(
           applyAts.get().getOutput().get().getAsFile().getAbsolutePath(),
           task.getOutput().get().getAsFile().toPath().getParent().toAbsolutePath().toString()
@@ -152,19 +167,18 @@ public class ClassicModdingPlugin implements Plugin<Project> {
     });
     TaskProvider<Copy> copySrc = tasks.register("copySrc", Copy.class, task -> {
       task.dependsOn(applyCompilerPatches);
-      Set<File> srcDirs = javaExt.getSourceSets().getByName("main").getJava().getSrcDirs();
-      task.into(srcDirs.iterator().next());
+      task.into(mcSourceSet.getJava().getSrcDirs().iterator().next());
       task.from(applyCompilerPatches.get().getOutput());
       task.setDuplicatesStrategy(DuplicatesStrategy.INCLUDE);
     });
     tasks.register("genPatches", Differ.class, task -> {
-      task.dependsOn("applyCompilerPatches");
+      task.dependsOn(copySrc);
       task.getBackupSrcDir().set(applyCompilerPatches.get().getOutput());
       task.getModifiedSrcDir().set(copySrc.get().getDestinationDir());
     });
     project.afterEvaluate(project1 -> {
       ResourceRepo.init(project1);
-      project.getDependencies().add("implementation", "com.mojang:minecraft-assets:" + ext.getVersion().get());
+      project.getDependencies().add("mcImplementation", "com.mojang:minecraft-assets:" + ext.getVersion().get());
     });
   }
 }
