4aaf9d464ed79ffce47e01334d1e4987e83cec6f
heisluft <heisluftlp@gmail.com>
2022-07-16 23:17:31 +0200

    Add option to specify additional paths to bsl runtime classpath

--- src/main/java/de/heisluft/modding/tasks/CPFileDecorator.java
+++ src/main/java/de/heisluft/modding/tasks/CPFileDecorator.java
@@ -2,32 +2,36 @@ package de.heisluft.modding.tasks;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.RegularFileProperty;
-import org.gradle.api.tasks.OutputFile;
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.provider.SetProperty;
+import org.gradle.api.tasks.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public abstract class CPFileDecorator extends DefaultTask {
   @OutputFile
   public abstract RegularFileProperty getOutput();
 
+  @Input
+  public abstract SetProperty<File> getPaths();
+
   public CPFileDecorator() {
+    getPaths().convention(new HashSet<>());
     getOutput().convention(getProject().getLayout().getBuildDirectory().dir(getName()).map(dir -> dir.file("cp.txt")));
   }
 
   @TaskAction
   public void generate() throws IOException {
-    Files.write(
-        getOutput().getAsFile().get().toPath(),
-        getProject().getConfigurations().getByName("runtimeClasspath").getResolvedConfiguration().getFiles().stream()
-            .filter(f -> !f.getName().contains("-natives-"))
-            .map(File::getAbsolutePath).map(s -> s + "\n")
-            .collect(Collectors.joining())
-            .getBytes(StandardCharsets.UTF_8)
-    );
+    Set<File> effectivePaths = new HashSet<>(getPaths().get());
+
+    getProject().getConfigurations().getByName("runtimeClasspath").getResolvedConfiguration().getFiles().stream()
+        .filter(f -> !f.getName().contains("-natives-"))
+        .forEach(effectivePaths::add);
+
+    Files.write(getOutput().get().getAsFile().toPath(), effectivePaths.stream().map(File::getAbsolutePath).collect(Collectors.toList()));
   }
 }
