792f9dfbd6f2aa2e27a7027bb7198ee4103d7206
heisluft <heisluftlp@gmail.com>
2022-08-22 13:30:19 +0200

    Split up ClassicModdingPlugin into a abstract superclass and a subclass called JarModDevPlugin, create another subclass for developing mappings and decomp fixer patches

---
+++ src/main/java/de/heisluft/modding/plugins/DeobfDataDevPlugin.java
@@ -0,0 +1,121 @@
+package de.heisluft.modding.plugins;
+
+import de.heisluft.modding.tasks.Extract;
+import de.heisluft.modding.tasks.OutputtingJavaExec;
+import org.gradle.api.Project;
+import org.gradle.api.file.DuplicatesStrategy;
+import org.gradle.api.file.RegularFileProperty;
+import org.gradle.api.tasks.Copy;
+import org.gradle.api.tasks.TaskContainer;
+import org.gradle.api.tasks.TaskProvider;
+import org.gradle.jvm.toolchain.JavaLanguageVersion;
+import org.gradle.jvm.toolchain.JavaToolchainService;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class DeobfDataDevPlugin extends BasePlugin {
+    @Override
+    public void apply(Project project) {
+        super.apply(project);
+        TaskContainer tasks = project.getTasks();
+
+        tasks.getByName("classes").dependsOn(tasks.getByName(mcSourceSet.getClassesTaskName()));
+
+        // for task dependency management
+        OutputtingJavaExec fixConstructors = tasks.withType(OutputtingJavaExec.class).getByName("fixConstructors");
+
+        File deobfWorkspaceDir = project.file("deobf-workspace");
+        deobfWorkspaceDir.mkdirs();
+
+        TaskProvider<OutputtingJavaExec> gATT = tasks.register("genATsTemp", OutputtingJavaExec.class, task -> {
+            task.dependsOn(fixConstructors);
+            task.classpath(deobfToolsJarFile);
+            task.setOutputFilename("at.cfg");
+            task.getMainClass().set("de.heisluft.reveng.ATGenerator");
+            task.args(
+                    fixConstructors.getOutput().get().getAsFile().getAbsolutePath(),
+                    task.getOutput().get().getAsFile().getAbsolutePath()
+            );
+        });
+
+        TaskProvider<OutputtingJavaExec> aATT = tasks.register("applyATsTemp", OutputtingJavaExec.class, task -> {
+            File inFile = gATT.get().getOutput().get().getAsFile();
+            task.onlyIf(t -> inFile.exists());
+            task.dependsOn(gATT);
+            task.classpath(deobfToolsJarFile);
+            task.setOutputFilename("minecraft.jar");
+            task.getMainClass().set("de.heisluft.reveng.at.AtApplicator");
+            task.args(
+                    fixConstructors.getOutput().get().getAsFile().getAbsolutePath(),
+                    inFile.getAbsolutePath(),
+                    task.getOutput().get().getAsFile().getAbsolutePath()
+            );
+        });
+
+        TaskProvider<OutputtingJavaExec> gMT = tasks.register("genMappingsTemp", OutputtingJavaExec.class, task -> {
+            task.dependsOn(fixConstructors);
+            task.classpath(deobfToolsJarFile);
+            task.setOutputFilename("mappings-generated.frg");
+            task.getMainClass().set("de.heisluft.reveng.Remapper");
+            task.args(
+                    "map",
+                    fixConstructors.getOutput().get().getAsFile().getAbsolutePath(),
+                    task.getOutput().get().getAsFile().getAbsolutePath()
+            );
+            task.doLast("copyToMainDir", t -> {
+                File target = new File(deobfWorkspaceDir, "fergie.frg");
+                if(!target.isFile()) {
+                    try {
+                        Files.copy(task.getOutput().get().getAsFile().toPath(), target.toPath());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        });
+
+        TaskProvider<OutputtingJavaExec> rJFT = tasks.register("remapJarFrgTemp", OutputtingJavaExec.class, task -> {
+            task.getOutputs().upToDateWhen(t -> false);
+            task.dependsOn(gMT, aATT);
+            task.classpath(deobfToolsJarFile);
+            task.setOutputFilename("minecraft.jar");
+            task.getMainClass().set("de.heisluft.reveng.Remapper");
+
+            task.doFirst(t -> {
+                RegularFileProperty inFile = gATT.get().getOutput().get().getAsFile().exists() ? aATT.get().getOutput() : fixConstructors.getOutput();
+                task.args(
+                        "remap",
+                        inFile.get().getAsFile().getAbsolutePath(),
+                        new File(deobfWorkspaceDir, "fergie.frg").getAbsolutePath(),
+                        "-o",
+                        task.getOutput().get().getAsFile().getAbsolutePath()
+                );
+            });
+        });
+
+        TaskProvider<OutputtingJavaExec> decompMC = tasks.register("decompMC", OutputtingJavaExec.class, task -> {
+            task.dependsOn(rJFT);
+            task.setOutputFilename("minecraft.jar");
+            task.classpath(fernFlowerJarFile);
+            task.getMainClass().set("org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler");
+            task.setMaxHeapSize("4G");
+            task.getJavaLauncher().set(project.getExtensions().getByType(JavaToolchainService.class).launcherFor(v -> v.getLanguageVersion().set(JavaLanguageVersion.of(11))));
+            task.args(
+                    rJFT.get().getOutput().get().getAsFile().getAbsolutePath(),
+                    task.getOutput().get().getAsFile().toPath().getParent().toAbsolutePath().toString()
+            );
+        });
+        TaskProvider<Extract> extractSrc = tasks.register("extractSrc", Extract.class, task -> {
+            task.dependsOn(decompMC);
+            task.getInput().set(decompMC.get().getOutput());
+        });
+        tasks.register("copySrc", Copy.class, task -> {
+            task.dependsOn(extractSrc);
+            task.into(mcSourceSet.getJava().getSrcDirs().iterator().next());
+            task.from(extractSrc.get().getOutput());
+            task.setDuplicatesStrategy(DuplicatesStrategy.INCLUDE);
+        });
+    }
+}
---
+++ src/main/java/de/heisluft/modding/plugins/JarModDevPlugin.java
@@ -0,0 +1,98 @@
+package de.heisluft.modding.plugins;
+
+import de.heisluft.modding.Ext;
+import de.heisluft.modding.tasks.*;
+import org.gradle.api.Project;
+import org.gradle.api.file.DuplicatesStrategy;
+import org.gradle.api.tasks.*;
+import org.gradle.jvm.toolchain.JavaToolchainService;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class JarModDevPlugin extends BasePlugin {
+
+  @Override
+  public void apply(Project project) {
+    super.apply(project);
+    JavaToolchainService service = project.getExtensions().getByType(JavaToolchainService.class);
+    TaskContainer tasks = project.getTasks();
+
+    tasks.getByName("classes").dependsOn(tasks.getByName(mcSourceSet.getClassesTaskName()));
+    // for task dependency management
+    OutputtingJavaExec fixCtors = tasks.withType(OutputtingJavaExec.class).getByName("fixConstructors");
+
+    TaskProvider<MavenDownload> downloadDeobfData = tasks.register("downloadDeobfData", MavenDownload.class, task -> {
+      task.getGroupName().set("de.heisluft.deobf.data");
+      task.getArtifactName().set(project.getExtensions().getByType(Ext.class).getVersion());
+      task.getExtension().set("zip");
+      task.getMavenRepoUrl().set(REPO_URL);
+    });
+    TaskProvider<Extract> extractData = tasks.register("extractData", Extract.class, task -> {
+      task.dependsOn(downloadDeobfData);
+      task.getInput().set(downloadDeobfData.get().getOutput());
+      task.getIncludedPaths().addAll(Arrays.asList("fergie.frg", "at.cfg", "patches/*"));
+    });
+    TaskProvider<OutputtingJavaExec> remapJar = tasks.register("remapJar", OutputtingJavaExec.class, task -> {
+      task.dependsOn(extractData, fixCtors);
+      task.classpath(deobfToolsJarFile);
+      task.setOutputFilename("minecraft.jar");
+      task.getMainClass().set("de.heisluft.reveng.Remapper");
+      task.args(
+          "remap",
+          fixCtors.getOutput().get().getAsFile().getAbsolutePath(),
+          new File(extractData.get().getOutput().getAsFile().get(), "fergie.frg").getAbsolutePath(),
+          "-o",
+          task.getOutput().get().getAsFile().getAbsolutePath()
+      );
+    });
+    TaskProvider<Zip2ZipCopy> stripLibs = tasks.register("stripLibraries", Zip2ZipCopy.class, task -> {
+      task.dependsOn(remapJar);
+      task.getInput().set(remapJar.get().getOutput());
+      task.getIncludedPaths().addAll(Arrays.asList("util/**", "com/**"));
+    });
+    TaskProvider<OutputtingJavaExec> applyAts = tasks.register("applyAts", OutputtingJavaExec.class, task -> {
+      task.dependsOn(stripLibs);
+      task.classpath(deobfToolsJarFile);
+      task.setOutputFilename("minecraft.jar");
+      task.getMainClass().set("de.heisluft.reveng.at.ATApplicator");
+      task.args(
+          stripLibs.get().getOutput().getAsFile().get().getAbsolutePath(),
+          new File(extractData.get().getOutput().getAsFile().get(), "at.cfg"),
+          task.getOutput().getAsFile().get().getAbsolutePath()
+      );
+    });
+    TaskProvider<OutputtingJavaExec> decompMC = tasks.register("decompMC", OutputtingJavaExec.class, task -> {
+      task.dependsOn(applyAts);
+      task.setOutputFilename("minecraft.jar");
+      task.classpath(fernFlowerJarFile);
+      task.getMainClass().set("org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler");
+      task.setMaxHeapSize("4G");
+      task.getJavaLauncher().set(service.launcherFor(versionOf(11)));
+      task.args(
+          applyAts.get().getOutput().get().getAsFile().getAbsolutePath(),
+          task.getOutput().get().getAsFile().toPath().getParent().toAbsolutePath().toString()
+      );
+    });
+    TaskProvider<Extract> extractSrc = tasks.register("extractSrc", Extract.class, task -> {
+      task.dependsOn(decompMC);
+      task.getInput().set(decompMC.get().getOutput());
+    });
+    TaskProvider<Patcher> applyCompilerPatches = tasks.register("applyCompilerPatches", Patcher.class, task -> {
+      task.dependsOn(extractSrc);
+      task.getPatchDir().set(extractData.get().getOutput().dir("patches"));
+      task.getInput().set(extractSrc.get().getOutput());
+    });
+    TaskProvider<Copy> copySrc = tasks.register("copySrc", Copy.class, task -> {
+      task.dependsOn(applyCompilerPatches);
+      task.into(mcSourceSet.getJava().getSrcDirs().iterator().next());
+      task.from(applyCompilerPatches.get().getOutput());
+      task.setDuplicatesStrategy(DuplicatesStrategy.INCLUDE);
+    });
+    tasks.register("genPatches", Differ.class, task -> {
+      task.dependsOn(copySrc);
+      task.getBackupSrcDir().set(applyCompilerPatches.get().getOutput());
+      task.getModifiedSrcDir().set(copySrc.get().getDestinationDir());
+    });
+  }
+}
