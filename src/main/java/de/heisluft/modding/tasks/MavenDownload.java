5146d5fede579063c7f76570eb06d7a1f12534e6
heisluft <heisluftlp@gmail.com>
2022-10-27 16:58:59 +0200

    cleanup some imports

--- src/main/java/de/heisluft/modding/tasks/MavenDownload.java
+++ src/main/java/de/heisluft/modding/tasks/MavenDownload.java
@@ -10,8 +10,6 @@ import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
