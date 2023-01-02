46e05e6c06b3f63598afc41de7e4cdb5855e5bf8
heisluft <heisluftlp@gmail.com>
2023-01-02 10:57:37 +0100

    Delete unused extension

--- src/main/java/de/heisluft/modding/extensions/DeobfDataExt.java
+++
@@ -1,8 +0,0 @@
-package de.heisluft.modding.extensions;
-
-import org.gradle.api.file.DirectoryProperty;
-
-//TODO: Evaluate if one property is worth a separate extension
-public abstract class DeobfDataExt {
-    public abstract DirectoryProperty getDataCopyDestinationDir();
-}
